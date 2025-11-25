dc44edb3c1571acba0216edb7004a783f011ea3b
icegellycream <angeli17figurado@gmail.com>
2025-11-25 15:31:17 +0800

    Video 3 - Get properties in the front end and list them - Figurado, Angeli Marie L.

--- djangobnb_backend/settings.py
+++ djangobnb_backend/settings.py
@@ -52,6 +52,8 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://127.0.0.1:3000',
+    'http://localhost:3000',
+    'http://localhost:8000',
 ]
 
 REST_AUTH = {
