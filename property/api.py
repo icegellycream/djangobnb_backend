d4f7717857ff7f32c694cd1fac0be2ec141f5b76
icegellycream <angeli17figurado@gmail.com>
2025-12-03 13:38:44 +0800

    Video 5 - Add Properties(create model,API endpoint,send data) - Figurado, Angeli Marie L.

--- property/api.py
+++ property/api.py
@@ -1,9 +1,14 @@
 from django.http import JsonResponse
 
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 
+from .forms import PropertyForm
 from .models import Property
 from .serializers import PropertiesListSerializer
+
 
 @api_view(['GET'])
 @authentication_classes([])
@@ -13,5 +18,36 @@ def properties_list(request):
     serializer = PropertiesListSerializer(properties, many=True)
 
     return JsonResponse({
-        'data': serializer.data
+        'data': serializer.data 
     })
+
+@api_view(['POST'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
+def create_property(request):
+    form = PropertyForm(request.POST, request.FILES)
+
+    if form.is_valid():
+        property = form.save(commit=False)
+        property.landlord = request.user
+        property.save()
+
+        return JsonResponse({'success': True})
+    else:
+        print('error', form.errors, form.non_field_errors)
+        return JsonResponse({'errors': form.errors.as_json()}, status=400)
+
+
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def properties_detail(request, pk):
+    try:
+        property = Property.objects.get(pk=pk)
+        serializer = PropertiesListSerializer(property)
+        
+        return JsonResponse({
+            'data': serializer.data
+        })
+    except Property.DoesNotExist:
+        return JsonResponse({'error': 'Property not found'}, status=404)
