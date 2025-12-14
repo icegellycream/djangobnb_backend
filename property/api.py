9e92eff5ac7bf67aa27a35d963e0d6a797f3fe2c
icegellycream <angeli17figurado@gmail.com>
2025-12-14 09:16:48 +0800

    Video 8 - Search details(numbre of guests,bedrooms,etc) - Figurado,Angeli Marie L.

--- property/api.py
+++ property/api.py
@@ -1,3 +1,5 @@
+from asyncio import proactor_events
+from unicodedata import category
 from django.http import JsonResponse
 
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
@@ -34,12 +36,45 @@ def properties_list(request):
     is_favorites = request.GET.get('is_favorites', '')
     landlord_id = request.GET.get('landlord_id', '')
 
+    country = request.GET.get('country', '')
+    category = request.GET.get('category', '')
+    checkin_date = request.GET.get('checkIn_date', '')
+    checkout_date = request.GET.get('checkOut_date', '')
+    bedrooms = request.GET.get('numBedrooms', '')
+    guests = request.GET.get('numGuests', '')
+    bathrooms = request.GET.get('numBathrooms', '')
+
+    if checkin_date and checkout_date:
+        exact_matches = Reservation.objects.filter(start_date=checkin_date) | Reservation.objects.filter(end_date=checkout_date)
+        overlap_matches = Reservation.objects.filter(start_date__lte=checkout_date, end_date__gte=checkin_date)
+        all_matches = []
+
+        for reservation in exact_matches | overlap_matches:
+            all_matches.append(reservation.property_id)
+
+        properties = properties.exclude(id__in=all_matches)
+
     if landlord_id:
         properties = properties.filter(landlord_id=landlord_id)
 
     if is_favorites:
         properties = properties.filter(favorited__in=[user])
+
+    if guests:
+        properties = properties.filter(guests__gte=guests)
     
+    if bedrooms:
+        properties = properties.filter(bedrooms__gte=bedrooms)
+    
+    if bathrooms:
+        properties = properties.filter(bathrooms__gte=bathrooms)
+
+    if country:
+        properties = properties.filter(country=country)
+    
+    if category and category != 'undefined':
+        properties = properties.filter(category=category)
+
     #
     # Favorites
         
